19fe0dc8ad4949eea5e93ed2d1f28d6266935a69
Christer Sjöholm <hcs@furuvik.net>
2014-05-04 14:22:50 +0200

    Some default group docs and comments.

--- goobook/config.py
+++ goobook/config.py
@@ -37,7 +37,11 @@ TEMPLATE = '''\
 ;cache_expiry_hours: 24
 ;filter_groupless_contacts: yes
 # New contacts will be added to this group in addition to "My Contacts"
-;default_group: 
+# Note that the group has to already exist on google or an error will occur.
+# One use for this is to add new contacts to an "Unsorted" group, which can
+# be sorted easier than all of "My Contacts".
+    searching for groupless users is a bit painful.
+;default_group:
 '''
 
 def read_config(config_file):
